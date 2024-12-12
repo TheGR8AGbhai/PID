b2d3b0620189f21956fe3e2841cdeac44888ab34
TheGR8AGbhai <alokgarg003.ag@gmail.com>
2024-12-12 17:14:40 +0530

    API addition

---
+++ personal-info-app-API-main/Data/UserContext.cs
@@ -0,0 +1,21 @@
+﻿using PersonalInfoApi.Models;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace PersonalInfoApi.Data
+{
+    public class UserContext: DbContext
+    {
+        public UserContext(DbContextOptions<UserContext> options) : base(options) { }
+
+        public DbSet<UserDetails> UserDetails { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserDetails>()
+                .HasKey(ud => ud.ID);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
